be6554aefae3969a8aa05a63cda72ad448d6c28e
Jens Rijks <jens.rijks@student.hu.nl>
2022-01-22 19:43:44 +0100

    .

--- src/index.js
+++ src/index.js
@@ -2,34 +2,34 @@
 import './../node_modules/@vaadin/router/dist/vaadin-router.js';
 import './service/AppRouting.js';
 
-import './view/components/page-loader';
-import './view/components/course-info-cursuscoordinator';
-import './view/components/course-info-student';
-import './view/components/course-modal-cursuscoordinator';
-import './view/components/course-modal-student';
-import './view/components/course-table';
-import './view/components/dropdown-tabblad';
-import './view/components/export-button';
-import './view/components/export-import-worker';
-import './view/components/filter-courses';
-import './view/components/find-course';
-import './view/components/import-excel';
-import './view/components/info-modal';
-import './view/components/table-nav';
-import './view/components/login-button';
-import './view/components/signout-button';
-import './view/components/footer';
-import './view/components/crud-koppeling';
-import './view/components/add-conversiebezem';
+import './view/components/page-loader.js';
+import './view/components/course-info-cursuscoordinator.js';
+import './view/components/course-info-student.js';
+import './view/components/course-modal-cursuscoordinator.js';
+import './view/components/course-modal-student.js';
+import './view/components/course-table.js';
+import './view/components/dropdown-tabblad.js';
+import './view/components/export-button.js';
+import './view/components/export-import-worker.js';
+import './view/components/filter-courses.js';
+import './view/components/find-course.js';
+import './view/components/import-excel.js';
+import './view/components/info-modal.js';
+import './view/components/table-nav.js';
+import './view/components/login-button.js';
+import './view/components/signout-button.js';
+import './view/components/footer.js';
+import './view/components/crud-koppeling.js';
+import './view/components/add-conversiebezem.js';
 
 
-import './view/components/nav-barcursus';
-import './view/components/nav-bar-student';
+import './view/components/nav-barcursus.js';
+import './view/components/nav-bar-student.js';
 
-import './view/pages/page-not-found';
-import './view/pages/student-page';
-import './view/pages/cursuscoordinator-page';
-import './view/pages/crud-page';
+import './view/pages/page-not-found.js';
+import './view/pages/student-page.js';
+import './view/pages/cursuscoordinator-page.js';
+import './view/pages/crud-page.js';
 
 
 
